a37f49d197032d7c79f6213c7c6ceaa18e5d2762
Crom (Thibaut CHARLES) <CromFr@gmail.com>
2017-02-23 14:19:01 +0100

    proper svg animation

--- slides.js
+++ slides.js
@@ -1,46 +1,65 @@
-Reveal.addEventListener('linesofcodetimespent', function() {
 
 
-		var s = Snap("#linesofcodetimespent");
-		s.select("#frag1").animate({opacity: 0}, 0);
-		s.select("#frag2").animate({opacity: 0}, 0);
-		s.select("#frag3").animate({opacity: 0}, 0);
-		s.select("#frag4").animate({opacity: 0}, 0);
-	}, false);
+// Called by <object id='linesofcodetimespent'> onload
+function linesofcodetimespent_load(){
+	var s = Snap("#linesofcodetimespent");
+	s.select("#frag1").animate({opacity: 0}, 0);
+	s.select("#frag2").animate({opacity: 0}, 0);
+	s.select("#frag3").animate({opacity: 0}, 0);
+	s.select("#frag4").animate({opacity: 0}, 0);
+}
+
+var animations = {
+	linesofcodetimespent1: {
+		show: function(s){ s.select("#frag1").animate({opacity: 1}, 700); },
+		hide: function(s){ s.select("#frag1").animate({opacity: 0}, 700); },
+	},
+	linesofcodetimespent2: {
+		show: function(s){ s.select("#frag2").animate({opacity: 1}, 700); },
+		hide: function(s){ s.select("#frag2").animate({opacity: 0}, 700); },
+	},
+	linesofcodetimespent3: {
+		show: function(s){ s.select("#frag3").animate({opacity: 1}, 700); },
+		hide: function(s){ s.select("#frag3").animate({opacity: 0}, 700); },
+	},
+	linesofcodetimespent4: {
+		show: function(s){ s.select("#frag4").animate({opacity: 1}, 700); },
+		hide: function(s){ s.select("#frag4").animate({opacity: 0}, 700); },
+	},
+	linesofcodetimespent5: {
+		show: function(s){
+			s.select("#frag1").animate({opacity: 0}, 300);
+			s.select("#frag2").animate({opacity: 0}, 300);
+			s.select("#frag3").animate({opacity: 0}, 300);
+			s.select("#frag4").animate({opacity: 0}, 300);
+			s.select("#curve").animate({d : "m 5.2916665,23 c 10,10  20,10  57,10"}, 3000, mina.easeinout);
+		},
+		hide: function(s){
+			s.select("#frag1").animate({opacity: 1}, 300);
+			s.select("#frag2").animate({opacity: 1}, 300);
+			s.select("#frag3").animate({opacity: 1}, 300);
+			s.select("#frag4").animate({opacity: 1}, 300);
+			s.select("#curve").animate({d : "m5.29 33.4c21-4 51.4-8.98 57.4-33"}, 1000, mina.easeinout);
+		},
+	},
+}
 
 Reveal.addEventListener('ready', function(event) {
 
 
 
 	Reveal.addEventListener('fragmentshown', function(event) {
-		// event.fragment = the fragment DOM element
-		console.log("fragment: ", event);
-
-		switch (event.fragment.id) {
-			case "linesofcodetimespent1":
-				var s = Snap("#linesofcodetimespent");
-				s.select("#frag1").animate({opacity: 1}, 700);
-				break;
-			case "linesofcodetimespent2":
-				var s = Snap("#linesofcodetimespent");
-				s.select("#frag2").animate({opacity: 1}, 700);
-				break;
-			case "linesofcodetimespent3":
-				var s = Snap("#linesofcodetimespent");
-				s.select("#frag3").animate({opacity: 1}, 700);
-				break;
-			case "linesofcodetimespent4":
-				var s = Snap("#linesofcodetimespent");
-				s.select("#frag4").animate({opacity: 1}, 700);
-				break;
-			case "linesofcodetimespent5":
-				var s = Snap("#linesofcodetimespent");
-				s.select("#frag1").animate({opacity: 0}, 300);
-				s.select("#frag2").animate({opacity: 0}, 300);
-				s.select("#frag3").animate({opacity: 0}, 300);
-				s.select("#frag4").animate({opacity: 0}, 300);
-				s.select("#curve").animate({d : "m 5.2916665,23 c 10,10  20,10  57,10"}, 3000, mina.easeinout);
-				break;
+		if(event.fragment.id in animations){
+			var svg_id = event.fragment.id.replace(/\d+$/, "");
+			var s = Snap("#"+svg_id);
+			animations[event.fragment.id].show(s);
+		}
+	});
+	Reveal.addEventListener('fragmenthidden', function(event) {
+		if(event.fragment.id in animations){
+			var svg_id = event.fragment.id.replace(/\d+$/, "");
+			var s = Snap("#"+svg_id);
+			animations[event.fragment.id].hide(s);
 		}
 	});
 });
--- slides.md
+++ slides.md
@@ -59,13 +59,12 @@ Note: You need to be informed when & where it occurs, and handle it
 
 
 ## Time wasted
-<object id="linesofcodetimespent" data="res/linesofcodetimespent.svg" type="image/svg+xml"><!-- .element: class="raw full-height" -->
+<object id="linesofcodetimespent" data="res/linesofcodetimespent.svg" type="image/svg+xml" onload="linesofcodetimespent_load()"><!-- .element: class="raw full-height" -->
 <span id="linesofcodetimespent1" class="fragment"></span>
 <span id="linesofcodetimespent2" class="fragment"></span>
 <span id="linesofcodetimespent3" class="fragment"></span>
 <span id="linesofcodetimespent4" class="fragment"></span>
 <span id="linesofcodetimespent5" class="fragment"></span>
-<!-- .slide: data-state="linesofcodetimespent" -->
 
 
 ## True story: LCDA
